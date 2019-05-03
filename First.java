2ed3ee2d3439d3d431f520cabf0c7a128079b26f
amulyavm <amulya.mu@gmail.com>
2019-05-03 12:55:00 -0500

    t

---
+++ First.java
@@ -0,0 +1,23 @@
+package firstProject2;
+
+public class First {
+
+	public static void main(String[] args) {
+		int a=10; 
+		int b=5;
+		int c = a/b;
+		System.out.println(c);
+		
+		multi();
+	}
+	
+	
+	public static void multi() {
+		int x=21;
+		int y=40;
+		int z = x*y; 
+	System.out.println(z);
+		
+		// TODO}
+
+}}
