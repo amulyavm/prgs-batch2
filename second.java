ca48a5df8d5dc4459ef157e7c6e5c10c25d56571
amulyavm <amulya.mu@gmail.com>
2019-05-03 14:24:55 -0500

    Changes to be committed:

---
+++ second.java
@@ -0,0 +1,16 @@
+package firstProject2;
+
+public class second {
+
+	public static void main(String[] args) {
+		
+		for (int x=1;x<=5;x++)
+		{
+			System.out.println("line" + x);
+		
+		// TODO Auto-generated method stub
+
+	}
+	}
+	
+}
