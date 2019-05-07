689d4e8f1a9123530f3686fd4ecbc9522db93cc4
amulyavm <amulya.mu@gmail.com>
2019-05-06 21:46:59 -0500

    add first.java class

--- First.java
+++ First.java
@@ -3,21 +3,38 @@ package firstProject2;
 public class First {
 
 	public static void main(String[] args) {
+		System.out.println("this is main start");
 		int a=10; 
 		int b=5;
 		int c = a/b;
-		System.out.println(c);
-		
+		System.out.println("division is "+ c);
+		System.out.println("this is multiplaction ");
 		multi();
+		System.out.println("this is end of multi method");
+		add();
+		System.out.println("this is end");
+		
 	}
 	
-	
 	public static void multi() {
+		System.out.println("this is multi ");
 		int x=21;
 		int y=40;
 		int z = x*y; 
-	System.out.println(z);
-		
+	System.out.println("multiplication is " + z);
+	System.out.println("end of multi");	
 		// TODO}
+	
+	
+	}
+	public static void add() {
+		System.  out. println("this is start of addition");
+		int d=21;
+		int e= 31;
+		int f=d+e;
+		System.out.println("addition is "+ f);
+		System.out.println("this is the end of addition");
+		
+	}
 
-}}
+}
